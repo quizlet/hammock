a651309318a02825244198ffbd2319fd7460fd30
Lexidor Digital <31805625+lexidor@users.noreply.github.com>
2020-02-21 09:58:54 +0100

    Use the same version detection as fb_intercept

--- .travis.sh
+++ .travis.sh
@@ -10,15 +10,27 @@ php --version
   curl https://getcomposer.org/installer | php -- --install-dir=/usr/local/bin --filename=composer
 )
 
-runtime=$(hhvm --php -r "echo HHVM_VERSION[0] === '3' ? 'hhvm' : 'php';")
+runtime=$(hhvm --php -r "echo HHVM_VERSION_ID >= 40000 ? 'php' : 'hhvm';")
 if [ "$runtime" = "hhvm" ]; then
+  removetestfiles=$(hhvm --php -r "echo HHVM_VERSION_ID < 32800 ? 'yes' : 'no';")
+  if [ "$removetestfiles" = "yes" ]; then
+    # The tests won't typecheck and work, so I remove them.
+    rm -r tests
+    # hhvm-autoload needs to have a directory here, but it can be empty.
+    mkdir tests
+    # We can't install hacktest
+    # rm composer.json (next commit)
+    # mv composer.old.json composer.json (next commit)
+  fi
   hhvm /usr/local/bin/composer install
 else
-  php /usr/local/bin/composer install
+    # Implicitly uses php
+    composer install
 fi
 
 hh_client
 
-vendor/bin/hacktest tests/
-
-vendor/bin/hhast-lint
+runstests=$(hhvm --php -r "echo HHVM_VERSION_ID >= 32800 ? 'canruntests' : 'cannotruntests';")
+if [ "$runstests" = "canruntests" ]; then
+  vendor/bin/hacktest tests/
+fi
