5e947056ba016a621c69ff935222c679a71a3a6a
Chris Opperwall <copperwall@gmail.com>
2020-03-22 11:17:18 -0700

    feat: only remove mocks every PRUNE_INTERVAL attempts past REGISTRY_SOFT_LIMIT

--- src/Persistent/PersistentMockRegistry.php
+++ src/Persistent/PersistentMockRegistry.php
@@ -7,16 +7,25 @@ use type Hammock\Interfaces\IDeactivatable;
 
 class PersistentMockRegistry {
 	const REGISTRY_SOFT_LIMIT = 100;
+	const PRUNE_INTERVAL = 10;
 	protected static vec<IDeactivatable> $registry = vec[];
+	protected static int $pruneAttempts = 0;
 
 	public static function register(IDeactivatable $persistentMock): void {
-		// When the registry grows beyond REGISTRY_SOFT_LIMIT size,
-		// we prune the registry by removing persistent mocks
-		// that have been manually deactivated.
 		self::$registry[] = $persistentMock;
 
-		if (C\count(self::$registry) >= self::REGISTRY_SOFT_LIMIT) {
+		// When the registry grows beyond REGISTRY_SOFT_LIMIT size,
+		// we prune the registry every PRUNE_INTERVAL calls to register
+		// by removing persistent mocks that have been manually deactivated.
+		//
+		// If the number of items in the registry drops below REGISTRY_SOFT_LIMIT,
+		// the number of attempts is reset to zero.
+		if (C\count(self::$registry) >= self::REGISTRY_SOFT_LIMIT &&
+			  self::$pruneAttempts % self::PRUNE_INTERVAL == 0) {
 			self::$registry = Vec\filter(self::$registry, $mock ==> !$mock->isDeactivated());
+			self::$pruneAttempts += 1;
+		} else {
+			self::$pruneAttempts = 0;
 		}
 	}
 
