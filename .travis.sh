fb27a8f6ca3d8a39ec3abf46d71bbed9f3d8ab94
Lexidor Digital <31805625+lexidor@users.noreply.github.com>
2020-03-23 20:31:26 +0100

    Rerun travis on an unchanged branch

--- .travis.sh
+++ .travis.sh
@@ -15,7 +15,7 @@ if [ "$canruntests" = "yes" ]; then
   rm composer.json
   mv composer.development.json composer.json
 else
-  # The tests won't typecheck and work, so I remove them.
+  # The tests won't typecheck and work, so we remove them.
   rm -r tests
   # hhvm-autoload needs to have a directory here, but it can be empty.
   mkdir tests
